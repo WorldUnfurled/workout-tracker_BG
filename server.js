b2581fd751a303306a73c794037499668127b7d0
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-08-15 17:03:33 -0400

    Added listen method in server

--- server.js
+++ server.js
@@ -10,3 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+app.listen(PORT, () => {
+    console.log(`Utilizing port ${PORT}.`)
+})
