6c08e6f2f1681ebc45c2092b5a4b2052c5fcfe4b
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-08-15 17:48:32 -0400

    Added date object to workout schema and empty excercises

--- models/Workout.js
+++ models/Workout.js
@@ -2,4 +2,10 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({});
+const WorkoutSchema = new Schema({
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: []
+});
