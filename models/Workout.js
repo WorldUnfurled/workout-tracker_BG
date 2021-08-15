1137c06c54ca65ec313bfff4631aba4b12a2f355
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-08-15 17:49:55 -0400

    added name, type, distance (cardio only), weight, sets, reps, and duration objects to exercises array with appropriate types

--- models/Workout.js
+++ models/Workout.js
@@ -7,5 +7,33 @@ const WorkoutSchema = new Schema({
         type: Date,
         default: Date.now
     },
-    exercises: []
+    exercises: [
+        {
+            name: {
+                type: String
+            },
+            type: {
+                type: String
+            },
+            distance: { // Optional if cardio
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            duration: {
+                type: Number
+            }
+        }
+    ]
 });
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = { Workout }
