8c3a4fbc1948538606c8f4083f9a5bf8a2ce4d0a
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-08-15 23:54:38 -0400

    Added empty workout post and get routes based on user story (Two add, two view)

--- routes/api/workoutRoutes.js
+++ routes/api/workoutRoutes.js
@@ -1,2 +1,7 @@
 const router = require('express').Router();
 const db = require('../../models');
+
+router.post("/workout", (req, res) => {});
+router.post("/workout", (req, res) => {});
+router.get("/workout", (req, res) => {});
+router.get("/workout", (req, res) => {});
