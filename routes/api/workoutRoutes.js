47861c610ccc0097fbc656d838d163be6ad5eb67
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-08-16 00:35:52 -0400

    Renamed first get api get method url /workout/week

--- routes/api/workoutRoutes.js
+++ routes/api/workoutRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const db = require('../../models');
 
-router.get("/workout", async (req, res) => { // View total duration for each of past seven workouts
+router.get("/workout/week", async (req, res) => { // View total duration for each of past seven workouts
     try {
         const wktAgg = db.Workout.aggregate([
             {
@@ -11,18 +11,22 @@ router.get("/workout", async (req, res) => { // View total duration for each of 
                 }
             },
         ]);
+
         const workout = await wktAgg.find().limit(7);
         res.json(workout);
     } catch (err) {
         res.json(err);
     }
 });
+
 router.get("/workout", (req, res) => { // View combined weight for each of past seven workouts
 
 });
+
 router.post("/workout", (req, res) => { // Add exercises to most recent workout plan
 
 });
+
 router.post("/workout", (req, res) => { // Add new exercises to new workout plan
 
 });
