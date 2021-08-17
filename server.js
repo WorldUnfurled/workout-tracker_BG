3ee8e367bf890fd0ba7b6ed2c777b932c7285e6c
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-08-17 08:34:44 -0400

    Changed URI to a heroku env variable to connect to mongoDB

--- server.js
+++ server.js
@@ -13,13 +13,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect('mongodb://localhost/workouts', {
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri, {
   useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 });
-
-
 
 app.use(require("./routes"));
 
